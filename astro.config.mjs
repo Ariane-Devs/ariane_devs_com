7de1d6930b20423dc82cf5683d2f75926c133e07
JSRamirezDev <sebastian@jsramirez.com>
2024-11-08 21:27:31 -0500

    Agregar soporte para Netlify: actualizar configuración, dependencias y archivos de despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless'
-import dotenv from 'dotenv';
+
+import netlify from "@astrojs/netlify/functions";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
+  output: "server",
+  site: "https://RamirezSebastianJ.github.io",
+  base: "ariane-devs",
 });
